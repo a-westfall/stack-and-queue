39568f7a9aa70c289b7df82f906724085bb4c440
Alexis Westfall <aew134@uakron.edu>
2026-02-06 16:05:44 +0000

    Add implementation for push()

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,7 @@
 /*
     Implementation file for stack container.
 
-    Includes push, pop, empty and isFull.
+    Includes push, pop, status, empty and isFull.
 */
 
 #include <cassert>
@@ -11,6 +11,9 @@ const int SIZE = 6;
 
 // top of stack
 int tos = -1;
+
+// array stack
+int stack[SIZE];
 
 /*
     Check if there are no elements in the stack.
@@ -23,12 +26,41 @@ bool empty() {
     return tos == -1;
 }
 
+/*
+    Add an element to the top of stack.
+
+    @return success or failure of pushing the element
+    @retval 0 success
+    @retval -1 failure
+*/
+int push(int element) {
+
+    // fail if stack is full
+    if(tos >= (SIZE - 1))
+        return -1;
+    
+    // add element to tos
+    stack[++tos] = element;
+    
+    // return success code
+    return 0;
+}
+
 int main() {
 
+    // empty stack
     {
-        int stack[SIZE];
-
         assert(empty());
+    }
+
+    // push an element
+    {
+        push(1);
+
+        assert(!empty());
+        assert(tos == 0);
+        assert(stack[0] == 1);
+
     }
 
     return 0;
