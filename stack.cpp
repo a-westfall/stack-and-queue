828ce28d65e4755e5f9992629cbdec0da7287636
Alexis Westfall <aew134@uakron.edu>
2026-02-06 16:15:09 +0000

    Add implementation for isFull()

--- stack.cpp
+++ stack.cpp
@@ -27,6 +27,17 @@ bool empty() {
 }
 
 /*
+    Check if the stack has the max number of elements.
+
+    @return whether the stack is full
+    @retval true stack is full
+    @retval false stack is not full
+*/
+bool isFull() {
+    return tos >= SIZE - 1;
+}
+
+/*
     Add an element to the top of stack.
 
     @return success or failure of pushing the element
@@ -36,7 +47,7 @@ bool empty() {
 int push(int element) {
 
     // fail if stack is full
-    if(tos >= (SIZE - 1))
+    if(isFull())
         return -1;
     
     // add element to tos
@@ -46,22 +57,52 @@ int push(int element) {
     return 0;
 }
 
+/*
+    Remove the top element from the stack.
+
+    @return success or failure of removing the element
+    @retval 0 success
+    @retval -1 failure
+*/
+int pop() {
+
+    // fail if stack is empty
+    if(empty())
+        return -1;
+    
+    // pop the element at tos
+    stack[tos--] = 0;
+
+    // return success code
+    return 0;
+}
+
 int main() {
 
     // empty stack
-    {
-        assert(empty());
-    }
+    assert(empty());
 
     // push an element
-    {
-        push(1);
+    push(1);
 
-        assert(!empty());
-        assert(tos == 0);
-        assert(stack[0] == 1);
+    assert(!empty());
+    assert(tos == 0);
+    assert(stack[0] == 1);
 
-    }
+    // pop an element
+    pop();
+
+    assert(empty());
+
+    // full stack
+    push(1);
+    push(2);
+    push(3);
+    push(4);
+    push(5);
+    push(6);
+
+    assert(isFull());
 
     return 0;
 }
