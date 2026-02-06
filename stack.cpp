2bbfde2e95a13d7e46401e235227b044acface03
Alexis Westfall <aew134@uakron.edu>
2026-02-06 15:54:23 +0000

    Add implementation for empty()

--- stack.cpp
+++ stack.cpp
@@ -4,13 +4,32 @@
     Includes push, pop, empty and isFull.
 */
 
+#include <cassert>
+
 // stack size
 const int SIZE = 6;
 
 // top of stack
 int tos = -1;
 
+/*
+    Check if there are no elements in the stack.
+
+    @return whether the stack is empty
+    @retval true stack empty
+    @retval false at least on element in stack
+*/
+bool empty() {
+    return tos == -1;
+}
+
 int main() {
+
+    {
+        int stack[SIZE];
+
+        assert(empty());
+    }
 
     return 0;
 }
