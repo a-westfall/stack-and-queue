c248dbb4f83aa74db94858b8e535b25d58236c81
Alexis Westfall <aew134@uakron.edu>
2026-02-06 17:03:23 +0000

    Add variables needed for queue implementation

--- queue.cpp
+++ queue.cpp
@@ -3,3 +3,16 @@
 
     Includes enqueue, dequeue, status, empty and isFull.
 */
+
+// queue size
+const int SIZE = 6;
+
+// meta-pointers for front and back of queue
+int head = 0;
+int tail = 0;
+
+// tracks whether queue is empty
+bool empty = true;
+
+// array queue
+int queue[SIZE];
