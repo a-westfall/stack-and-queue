4c690a254e096b4a8671b82d74f4ee76abf2c744
Alexis Westfall <aew134@uakron.edu>
2026-02-06 17:07:49 +0000

    Add implementation for empty()

--- queue.cpp
+++ queue.cpp
@@ -4,6 +4,8 @@
     Includes enqueue, dequeue, status, empty and isFull.
 */
 
+#include <cassert>
+
 // queue size
 const int SIZE = 6;
 
@@ -12,7 +14,26 @@ int head = 0;
 int tail = 0;
 
 // tracks whether queue is empty
-bool empty = true;
+bool isEmpty = true;
 
 // array queue
 int queue[SIZE];
+
+/*
+    Check if there are no elements in the queue.
+
+    @return Whether the queue is empty
+    @retval true Queue empty
+    @retval false At least on element in queue
+*/
+bool empty() {
+    return isEmpty;
+}
+
+int main() {
+
+    // empty queue
+    assert(empty());
+
+    return 0;
+}
